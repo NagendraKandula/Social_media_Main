79af32c575f63405a53bc8be3411f0f778a7df42
NagendraKandula <nagendra14902@gmail.com>
2025-12-01 19:51:19 +0530

    path changed

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -5,14 +5,14 @@ import { PrismaService } from './prisma/prisma.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { YoutubeModule } from './auth/youtube/youtube.module';
-import { FacebookModule } from './facebook/facebook.module';
+import { YoutubeModule } from './social_media_platforms/youtube/youtube.module';
+import { FacebookModule } from './social_media_platforms/facebook/facebook.module';
 import { YoutubeAnalyticsModule } from './youtube-analytics/youtube-analytics.module';
 import { AiAssistantModule } from './ai-assistant/ai-assistant.module';
-import { InstagramModule } from './instagram/instagram.module';
-import { TwitterModule } from './auth/twitter/twitter.module';
-import { ThreadsModule } from './auth/threads/threads.module';
-import { InstagramBusinessModule } from './auth/instagram-business/instagram-business.module';
+import { InstagramModule } from './social_media_platforms/instagram/instagram.module';
+import { TwitterModule } from './social_media_platforms/twitter/twitter.module';
+import { ThreadsModule } from './social_media_platforms/threads/threads.module';
+import { InstagramBusinessModule } from './social_media_platforms/instagram-business/instagram-business.module';
 
 @Module({
   imports: [
--- Backend/src/social_media_platforms/youtube/youtube.module.ts
+++ Backend/src/social_media_platforms/youtube/youtube.module.ts
@@ -2,7 +2,7 @@ import {Module } from '@nestjs/common';
 import {YoutubeController} from './youtube.controller';
 import {YoutubeService} from './youtube.service';
 import {PrismaModule} from '../../prisma/prisma.module';
-import { AuthModule } from '../auth.module';
+import { AuthModule } from '../../auth/auth.module';
 @Module({
     imports: [PrismaModule,AuthModule],
     controllers: [YoutubeController],
