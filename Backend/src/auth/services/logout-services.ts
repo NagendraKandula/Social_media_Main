61075d779b4bf9e7fe3d8f47e8bc98fe576ddeb4
NagendraKandula <nagendra14902@gmail.com>
2025-12-23 07:51:42 +0530

    facebook logout

---
+++ Backend/src/auth/services/logout-services.ts
@@ -0,0 +1,54 @@
+import { Injectable,BadRequestException,InternalServerErrorException } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import axios from "axios";
+@Injectable()
+export class LogoutService {
+  constructor(private readonly prisma: PrismaService) {}
+  async getFacebookProfile(userId: number) {
+    const account = await this.prisma.socialAccount.findFirst({
+        where: {
+            userId: userId,
+            provider: 'facebook',
+        },
+    });
+
+    if (!account || !account.accessToken) {
+        return null;
+    }
+    try{
+        const response = await axios.get('https://graph.facebook.com/me', {
+            params: {
+                fields: 'id,name,picture.type(large)',
+                access_token: account.accessToken,
+            },
+        });
+        return {
+            providerId: response.data.id,
+            name: response.data.name,
+            profilePicture: response.data.picture?.data?.url || null,
+            connected: true,
+        };
+    }
+    catch (error) {
+        
+        console.error('Error fetching Facebook profile:', error.response?.data || error.message);
+      return {
+        providerId: account.providerId,
+        name: 'Facebook User',
+        connected: false,
+        needsReconnect: true,
+      };
+     }
+  }
+  async disconnectProvider(userId: number, provider: string) {
+    return this.prisma.socialAccount.deleteMany({
+      where: {
+        userId: userId,
+        provider: provider,
+      },
+    });
+  }
+}
+
+        
+    
